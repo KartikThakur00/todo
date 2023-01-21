8c00c4809ed9ecb041ef08e850e7ad681b195809
Kartik Thakur <76154014+KartikThakur00@users.noreply.github.com>
2023-01-22 00:40:12 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Header() {
   return (
     <div className='todo-text-input'>
-            <textarea></textarea>
+          { <input name="title" type="text"  placeholder='Title'/>}
+            <textarea name='content' placeholder='Take a note...' spellCheck="false"></textarea>
+            <button >+</button>
     </div>
   )
 }
