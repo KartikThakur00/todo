94c14c1e13a92a9b128c9e5613d24cd678314302
Kartik Thakur <76154014+KartikThakur00@users.noreply.github.com>
2023-01-23 22:17:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import TodoElement from './components/TodoElement';
+import { nanoid } from 'nanoid';
 import './App.css';
 
 function App() {
@@ -9,18 +10,19 @@ function App() {
     title: "",
     content: ""
   })
+
   const [todo, setTodo] = useState(
     JSON.parse(localStorage.getItem("todo")) ||
     [{
+      id: nanoid(),
       titleData: "Demo element",
       contentData: "this is a demo element"
     }])
-  console.log("hello")
 
   useEffect(() => {
     localStorage.setItem("todo", JSON.stringify(todo))
     console.log("useEffect running")
-  },[todo])
+  }, [todo])
 
   function handleInputChange(e) {
     const { name, value } = e.target
@@ -32,10 +34,11 @@ function App() {
 
   function handleAdd() {
     setTodo(prevTodo => {
-      const arr=[{
+      const arr = [{
+        id: nanoid(),
         titleData: text.title,
         contentData: text.content
-      },...prevTodo]
+      }, ...prevTodo]
       return arr
     })
     setText({
@@ -43,10 +46,18 @@ function App() {
       content: ""
     })
   }
+
+  function handleDelete(todoId) {
+    console.log(todoId)
+    setTodo(prevTodo => (prevTodo.filter(todo => todo.id != todoId)))
+  }
+
   const todoList = todo.map(x => (
-    <TodoElement t={x.titleData} c={x.contentData} />
+    <TodoElement key={x.id} id={x.id} t={x.titleData} c={x.contentData} deleteHandler={() => handleDelete(x.id)} />
   ))
+
   console.log(todo)
+
   return (
     <div className="app">
       <Header
